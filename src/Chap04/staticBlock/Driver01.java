befefe2875002a105aca747eec2089b51643c9f5
YoonSung Kim <opqudk@gmail.com>
2020-03-14 07:36:54 +0900

    static block

---
+++ src/Chap04/staticBlock/Driver01.java
@@ -0,0 +1,7 @@
+package Chap04.staticBlock;
+
+public class Driver01 {
+    public static void main(String[] args) {
+        동물 뽀로로 = new 동물();
+    }
+}
---
+++ src/Chap04/staticBlock/Driver04.java
@@ -0,0 +1,10 @@
+package Chap04.staticBlock;
+
+public class Driver04 {
+    public static void main(String[] args) {
+        System.out.println("메인 메서드 시작");
+
+        동물 뽀로로 = new 동물();
+        동물 피카츄 = new 동물();
+    }
+}
