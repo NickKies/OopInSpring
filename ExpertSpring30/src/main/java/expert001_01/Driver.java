a124e75601c8c749616799ba5046ea69c73900fe
YoonSung Kim <opqudk@gmail.com>
2020-03-16 14:45:06 +0900

    DI

---
+++ ExpertSpring30/src/main/java/expert001_01/Driver.java
@@ -0,0 +1,9 @@
+package main.java.expert001_01;
+
+public class Driver {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        System.out.println(car.getTireBrand());
+    }
+}
