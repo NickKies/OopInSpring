297fdc1d0e6314366507aa903507a81b71e1d143
YoonSung Kim <opqudk@gmail.com>
2020-03-21 11:28:47 +0900

    DI

---
+++ ExpertSpring30/src/main/java/expert001_03/Driver.java
@@ -0,0 +1,13 @@
+package main.java.expert001_03;
+
+public class Driver {
+    public static void main(String[] args) {
+        Tire tire = new KoreaTire();
+        // Tire tire = new AmericaTire();
+
+        Car car = new Car();
+        car.setTire(tire);
+
+        System.out.println(car.getTireBrand());
+    }
+}
