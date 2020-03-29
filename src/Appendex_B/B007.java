d5e7622b039c7e8c3982a6aef6e536d546e16eac
YoonSung Kim <opqudk@gmail.com>
2020-03-29 10:03:09 +0900

    lambda

---
+++ src/Appendex_B/B007.java
@@ -0,0 +1,15 @@
+package Appendex_B;
+
+public class B007 {
+    public static void main(String[] args) {
+        MyFunctionalInterface mfi = a -> a * a;
+
+        doIt(mfi);
+    }
+
+    public static void doIt(MyFunctionalInterface mfi) {
+        int b = mfi.runSomething(5);
+
+        System.out.println(b);
+    }
+}
